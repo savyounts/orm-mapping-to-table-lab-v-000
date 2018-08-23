c7c2444d148596f8db1379412dd1116c189f3f87
Sav Younts <savyounts@gmail.com>
2018-08-23 03:19:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
       CREATE TABLE IF NOT EXISTS students(
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT,
+      grade TEXT
       )
       SQL
       DB[:conn].execute(sql)
